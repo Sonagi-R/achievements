87e6a41b99c315343b638ee5b9a84131ce3923b3
lewis <lewis.takavarasha@gmail.com>
2023-05-04 12:55:54 +0100

    add base for games page

--- src/components/Games/index.jsx
+++ src/components/Games/index.jsx
@@ -3,46 +3,222 @@ import "./index.css";
 
 export default function Games() {
   return (
-      <div className="games-container">
-          <div className="game">
-              <img src="https://assets-prd.ignimgs.com/2021/12/08/witcher3-1638987659679.jpg" alt="" />
-              <title></title>
-              <p>Random Text</p>
+    <div className="games-container my-5">
+      <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img className="card-img-top" src="https://assets-prd.ignimgs.com/2021/12/08/witcher3-1638987659679.jpg" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://image.api.playstation.com/cdn/EP1018/CUSA00135_00/e1lbaSUnmBzv5cqMyM9DjbcrG0mpMrHi.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+              </div>
           </div>
-          <div className="game">
-              <img src="https://gaming-cdn.com/images/products/2419/616x353/batman-arkham-knight-pc-game-steam-cover.jpg?v=1649319361" alt="" />
-              <title></title>
-              <p>Random Text</p>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://upload.wikimedia.org/wikipedia/en/4/47/Tom_Clancy%27s_Rainbow_Six_Siege_cover_art.jpg"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
           </div>
-          <div className="game">
-              <img src="https://prod.assets.earlygamecdn.com/images/csgo-2.jpg?mtime=1678019572" alt="" />
-              <title></title>
-              <p>Random Text</p>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://rocketleague.media.zestyio.com/rl_s10_rp_1-1_no-logo.BJdjMBSRj.jpg?optimize=high"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
           </div>
-          <div className="game">
-              <img src="https://rocketleague.media.zestyio.com/seasonylogostime.jpg?width=1440&optimize=high" alt="" />
-              <title></title>
-              <p>Random Text</p>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://assets1.ignimgs.com/2019/01/16/divinity-original-sin-2---button-fin-1547678837529.jpg"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
           </div>
-          <div className="game">
-              <img src="https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_DivinityOriginalSin2DefinitiveEdition_image1600w.jpg" alt="" />
-              <title></title>
-              <p>Random Text</p>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://image.api.playstation.com/vulcan/img/cfn/113075PxnarzRek4cRpjrRWSpLfrcBd23B5e_Yj2azms6nWYKmySv4h3a22G5_R1CM4BQUmSRD6oOArDROKv041NUkgun78-.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
           </div>
-          <div className="game">
-              <img src="https://image.api.playstation.com/vulcan/img/cfn/113075PxnarzRek4cRpjrRWSpLfrcBd23B5e_Yj2azms6nWYKmySv4h3a22G5_R1CM4BQUmSRD6oOArDROKv041NUkgun78-.png" alt="" />
-              <title></title>
-              <p>Random Text</p>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://www.gamespot.com/a/uploads/scale_tiny/1197/11970954/2431806-2227177-box_bl2.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
           </div>
-          <div className="game">
-              <img src="https://i.ytimg.com/vi/qyeDBQow1bI/maxresdefault.jpg" alt="" />
-              <title></title>
-              <p>Random Text</p>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://assets-prd.ignimgs.com/2022/01/05/rayman-3-button-1641344567706.jpg"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
           </div>
-          <div className="game">
-              <img src="https://assets-prd.ignimgs.com/2022/01/05/rayman-3-button-1641344567706.jpg" alt="" />
-              <title></title>
-              <p>Random Text</p>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img className="card-img-top" src="https://assets-prd.ignimgs.com/2021/12/08/witcher3-1638987659679.jpg" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://image.api.playstation.com/cdn/EP1018/CUSA00135_00/e1lbaSUnmBzv5cqMyM9DjbcrG0mpMrHi.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+              </div>
+          </div>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://upload.wikimedia.org/wikipedia/en/4/47/Tom_Clancy%27s_Rainbow_Six_Siege_cover_art.jpg"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+          </div>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://rocketleague.media.zestyio.com/rl_s10_rp_1-1_no-logo.BJdjMBSRj.jpg?optimize=high"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+          </div>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://assets1.ignimgs.com/2019/01/16/divinity-original-sin-2---button-fin-1547678837529.jpg"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+          </div>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://image.api.playstation.com/vulcan/img/cfn/113075PxnarzRek4cRpjrRWSpLfrcBd23B5e_Yj2azms6nWYKmySv4h3a22G5_R1CM4BQUmSRD6oOArDROKv041NUkgun78-.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+          </div>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://www.gamespot.com/a/uploads/scale_tiny/1197/11970954/2431806-2227177-box_bl2.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+          </div>
+          <div className="card p-0 game-card" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://assets-prd.ignimgs.com/2022/01/05/rayman-3-button-1641344567706.jpg"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some of the Card's content.</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
           </div>
     </div>
   );
