8ef9a63e9342cdb03cc5a9d107607344c93e6529
lewis <lewis.takavarasha@gmail.com>
2023-05-05 17:15:46 +0100

    add bootstrap rows and columsn to games page

--- src/components/Games/index.jsx
+++ src/components/Games/index.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import "./index.css";
 
 export default function Games() {
-
   const [games, setGames] = useState([]);
 
   useEffect(() => {
@@ -17,33 +16,47 @@ export default function Games() {
 
     setGames(data.results);
 
-    for(let i = 0; i < data.results.length; i++) {
+    for (let i = 0; i < data.results.length; i++) {
       console.log(data.results[i].name);
     }
   };
 
   return (
     <>
-      <h1 className="text-center">Dashboard</h1>
+
+        <h1 className="text-center">Dashboard</h1>
       <h2 className="text-center">Popular Games</h2>
-      <div className="games-container my-5">
-        {games.map((game) => (
-          <div className="card p-0 game-card" style={{ width: "18rem" }}>
-          <img className="card-img-top" src={game.background_image} alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">{game.name}</h5>
-            <p className="card-text">
-              {game.genres.map((genre) => (
-                <span>{genre.name} </span>
-              ))}
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-        ))}
+      <div className="row games-main">
+        <div className="col-2">
+          <h2 className="category-header">Categories</h2>
+          <div className="game-category">Action</div>
+          <div className="game-category">Adventure</div>
+          <div className="game-category">Platform</div>
+          <div className="game-category">Puzzle</div>
+          <div className="game-category">Shooter</div>
+          <div className="game-category">RPG</div>
       </div>
+      <div className="col-10">
+        <div className="games-container my-5">
+          {games.map((game) => (
+            <div className="card p-0 game-card" style={{ width: "18rem" }}>
+              <img className="card-img-top" src={game.background_image} alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title">{game.name}</h5>
+                <p className="card-text">
+                  {game.genres.map((genre) => (
+                    <span>{genre.name} </span>
+                  ))}
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+        </div>
+        </div>
     </>
   );
 }
