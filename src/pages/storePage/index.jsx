b27511a62e1cf9e47582c3037b1002e1d870625d
britishbambi <josefjakubiak@gmail.com>
2023-05-10 15:54:55 +0100

    Start store design

---
+++ src/pages/storePage/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function storePage() {
+  return (
+    <div>
+        <div className="card p-0 game-card " style={{ width: "18rem" }}>
+            <img className="card-img-top" src="" alt="store image" />
+            <div className="card-body">
+            <h5 className="card-title">Name</h5>
+            <p className="card-text">
+                text
+            </p>
+            <button>
+                Buy Now
+            </button>
+            </div>
+        </div> 
+    </div>
+  )
+}
